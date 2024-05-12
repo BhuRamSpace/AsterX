39413165fd4e8f0649979919c524a2b0d99ca993
SerenaSammiceli <ser.sammiceli@stud.uniroma3.it>
2024-05-12 18:48:31 +0200

    Problema doppia riga risolto

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -48,11 +48,15 @@ public class LeaderboardManager : MonoBehaviour
 
     void OnLeaderboardGet(GetLeaderboardResult result)
     {
+        foreach(Transform item in rowsParent){
+            Destroy(item.gameObject);
+        }
+
         foreach (var item in result.Leaderboard)
         {
             GameObject newGo = Instantiate(rowPrefab, rowsParent);
             Text[] texts = newGo.GetComponentsInChildren<Text>();
-            texts[0].text = item.Position.ToString();
+            texts[0].text = (item.Position + 1).ToString();
             texts[1].text = item.PlayFabId;
             texts[2].text = item.StatValue.ToString();
             Debug.Log(string.Format("PLACE:{0} | ID:{1} | VALUE:{2}",
